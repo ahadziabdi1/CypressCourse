2219f0c8180bf1c63c0831ad351051b24e9d617c
ahadziabdi1 <ahadziabdi1@etf.unsa.ba>
2025-01-23 21:20:20 +0100

    Add search for products starting with ca and proceed to checkout after adding one to cart

--- cypress/integration/examples/Test2.js
+++ cypress/integration/examples/Test2.js
@@ -1,19 +1,12 @@
-describe("My First Test", () => {
+describe("My Second Test", () => {
   it("Does not do much!", () => {
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
 
     cy.get(".search-keyword").type("ca");
     cy.wait(2000);
-    cy.get(".product:visible").should("have.length", 4);
 
     cy.get(".products").as("productLocator");
 
-    cy.get("@productLocator").find(".product").should("have.length", 4);
-    cy.get("@productLocator")
-      .find(".product")
-      .eq(2)
-      .contains("ADD TO CART")
-      .click();
     cy.get("@productLocator")
       .find(".product")
       .each(($el, index, $list) => {
@@ -22,9 +15,9 @@ describe("My First Test", () => {
           cy.wrap($el).find("button").click();
         }
       });
-    cy.get(".brand").should("have.text", "GREENKART");
-    cy.get(".brand").then(function (logoElement) {
-      cy.log(logoElement.text());
-    });
+
+    cy.get(".cart-icon > img").click();
+    cy.contains("PROCEED TO CHECKOUT").click();
+    cy.contains("Place Order").click();
   });
 });
