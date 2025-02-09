bbf530c1932ff576f1222b1957893653a44aff67
ahadziabdi1 <ahadziabdi1@etf.unsa.ba>
2025-02-09 21:05:29 +0100

    Add end-to-end test for submitting an order in the ecommerce application

---
+++ cypress/integration/examples/Test11.js
@@ -0,0 +1,36 @@
+describe("End to End ecommerce Test", () => {
+  it("Submit Order", () => {
+    cy.visit("https://rahulshettyacademy.com/loginpagePractise/#");
+    cy.get("#username").type("rahulshettyacademy");
+    cy.get("#password").type("learning");
+    cy.contains("Sign In").click();
+    cy.contains("Shop Name").should("be.visible");
+    cy.get("app-card").should("have.length", "4");
+
+    const productName = "Nokia Edge";
+    cy.get("app-card")
+      .filter(`:contains("${productName}")`)
+      .then(($element) => {
+        cy.wrap($element).should("have.length", "1");
+        cy.wrap($element).contains("button", "Add").click();
+      });
+
+    cy.get("app-card").eq(0).contains("button", "Add").click();
+    cy.get("#navbarResponsive > .navbar-nav > .nav-item > .nav-link").click();
+
+    let sum = 0;
+    cy.get("tr td:nth-child(4) strong")
+      .each(($el) => {
+        const amount = Number($el.text().split(" ")[1].trim());
+        sum = sum + amount;
+      })
+      .then(function () {
+        expect(sum).to.be.lessThan(200000);
+      });
+    cy.contains("button", "Checkout").click();
+    cy.get("#country").type("India");
+    cy.get(".suggestions > ul > li > a", { timeout: 10000 }).click();
+    cy.get(".ng-untouched > .btn").click();
+    cy.get(".alert-success").should("contain", "Success");
+  });
+});
