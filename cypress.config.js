4e639c0d07a9eec3affc0bbddfaf9c9010efc7ab
ahadziabdi1 <ahadziabdi1@etf.unsa.ba>
2025-02-26 09:42:58 +0100

    Add new screenshots for failed tests and update Cypress configuration with project ID and retry settings

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   defaultCommandTimeout: 6000,
   reporter: "cypress-mochawesome-reporter",
-
+  projectId: "pb8cyf",
+  retries: {
+    runMode: 1,
+  },
   env: {
     url: "https://google.com",
     userId: "rahul",
